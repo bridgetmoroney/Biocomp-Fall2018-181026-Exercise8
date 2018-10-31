7e8f94b0c622a5e67270031e205ecda0ed0551ee
Bridget Moroney <bmoroney@nd.edu>
2018-10-31 14:49:07 -0400

    Final Additions to Exercise 8

--- Exercise8.R
+++ Exercise8.R
@@ -1,89 +1,53 @@
 setwd("/Users/bridgetmoroney/Desktop/Biocomp-Fall2018-181026-Exercise8/")
+#setting working directory 
 countUW=0
+#setting a count for UW
 countMSU=0
+#setting a count for MSU
 scores=read.table("UWvMSU_1-22-13.txt", stringsAsFactors = F, header = T)
-for (i in 1:nrow(scores)){
-  if (scores$team[i]=="UW"){
-    countUW=0+scores$score[i]
-    countUW[i]=scores$score[i]+countUW
-    totalsUW=data.frame(time=scores$time, score=countUW[i])
-  }else{
-    countMSU=0+scores$score[i]
-    countMSU[i]=scores$score[i]+countMSU
-    totalsMSU=data.frame(time=scores$time, score=countMSU[i])
-  }
-}
-#do I need to unlist and define a previous variable?
-setwd("/Users/bridgetmoroney/Desktop/Biocomp-Fall2018-181026-Exercise8/")
-rm(list=ls())
-#second try
-countUW=0
-countMSU=0
-#defining empty counts for scores
-scores=read.table("UWvMSU_1-22-13.txt", stringsAsFactors = F, header = T)
-scores$cumulativeScoreUW=NA
-scores$cumulativeScoreMSU=NA
+#loading .txt file with scores 
+scores$cumulativeScore=NA
+#inserting empty column in the scores file to send for loop results to
 for (i in 1:nrow(scores)){
   #starting for loop to iterate over all rows of scores file
   if (scores$team[i]=="UW"){
     countUW=scores$score[i]+countUW
-    current=countUW+scores$score[i]
-    scores[i,4]=current
-    #adding to count if matches score for UW
+    scores[i,4]=countUW
+    #adding to count if team name in the row matches score for UW
   }else{
     countMSU=scores$score[i]+countMSU
-    current2=countMSU+scores$score[i]
-    scores[i,5]=current2
-    #adding to count if matches score for MSU
+    scores[i,4]=countMSU
+    #adding to count if team name in the row matches score for MSU
   }
 }
-plot=ggplot(data=scores,aes(x=time,y=cumulativeScoreUW,y=cumulativeScoreMSU))
-?plot
-plot(scores$time,scores$cumulativeScoreUW,type="p",xlab="Time (s)",ylab="Score")
-points(scores$time,scores$cumulativeScoreMSU)
-
-#just need to figure out how to get starting score accurate
-
-#asking user for guess
-random=sample(seq(1,100), size = 1)
-guess=readline(prompt="Input number guess:")
-#when the user types their answer in the console it will set the guess variable equal to that number
-for (i in random){
-  guess
-  if guess==random
-  print("correct!")
-}else{
-  if guess>random
-  print("greater")}
-}else{
-  if guess<random
-}
-}
-}
+plot=ggplot(data=scores,aes(x=time,y=cumulativeScore, color=team,group=team))
+#using ggplot to create a plot from scores data using new column as y axis and time as y
+#color and group tag create two distinct lines for each team's cumulative score
+plot+geom_line()+theme_classic()+xlab("Time (min)")+ylab("Cumulative Team Score")+ggtitle("MSU vs.UW Game")
+#creating line plot with more specific labels and a title
 
 
-#what do I index over and how to tell done after correct
 
-randomGuess=readline(prompt="Guess a number between 1 and 100:")
-input=0
-randomNumber=sample(seq(1,100),size=1)
-
-input=randomGuess
-while(input !=randomGuess){
-  if(input < randomNumber){
-    print("Higher!")
-  }else if(input > rand){
-    print("Lower!")
-  }else{
-    print("You've got it!")
+game=function(){
+  #defining the game as function so it can be run later
+  number=sample(1:100,1)
+  #setting a number that randomly samples between 1 and 100 every time game function is called
+  randomGuess=readline(prompt="Guess a number between 1 and 100:")
+  #creating prompt for user to set a number guess
+  while(randomGuess!=number){
+    #while loop to ensure guesses are only made if guess does not equal random number
+    if(randomGuess>number){
+      print("Lower")
+      randomGuess=readline(prompt="Guess again:")
+      #console will display the word lower if the user guesses too high and will prompt them for another guess
+    }else if(randomGuess<number){
+      print("Higher")
+      randomGuess=readline(prompt="Guess again:")
+      #console will display the word higher if the user guesses too low and will prompt them for another guess
+    }
   }
-  input = input +1
+  print("You did it!")
+  #printing you did it if the user guesses the correct number, the while condition is no longer met
 }
-#how do I get the game to work for user?
-
-
-
-
-
-
-
+game()
+#use to run the game function
